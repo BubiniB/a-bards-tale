e18208b23b7427e0bb046fa0da1fbb6e1bbc2877
BubiniB <cristina.ekqvist@outlook.com>
2023-04-17 06:55:28 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# This is my text adventure game:
+# A Bard's Tale
+# The code for this game is structured in such a way that each
+# room/scene the player is in, is its own function. For the main
+# function to work, the game flow is coded in reverse order
+# (the first scenes appear last in the code, the game end in the beginning).
+
+
+print("Let's go")
