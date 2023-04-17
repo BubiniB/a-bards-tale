e8ea7ba891c5fd3ec42df7f089b32aea5728c109
BubiniB <cristina.ekqvist@outlook.com>
2023-04-17 11:27:40 +0000

    Add room_one function with input validation

--- run.py
+++ run.py
@@ -6,9 +6,46 @@
 # (the first scenes appear last in the code, the game end in the beginning).
 
 def room_one():
-    print("bajs")
+    """
+    Let's players choose which directions to go
+    and validates input.
+    """
+    print("You have finally entered the evil witche's")
+    print("lair. You follow the path deeper into")
+    print("the cave until you come to a crossroad.\n")
+    print("The path has now split into two:\n")
+    print("You can continue to the right,")
+    print("or simply move onward.\n")
+    print("Which path will you choose?")
+
+    while True:
+        directions_room_one = input("Please choose (right/up): \n")
+        try:
+            if directions_room_one.lower() == "right":
+                print("You decide to explore the path")
+                print("to your right.")
+                room_two()
+                break
+            elif directions_room_one.lower() == "up":
+                print("You decide to continue onward.")
+                room_three()
+                break
+            elif directions_room_one.lower() == "left":
+                print("Maybe you should try the left!")
+                print("You hit your head on the wall.")
+            elif directions_room_one.lower() == "down":
+                print("No! You cannot turn back now!")
+            else:
+                raise ValueError("This is not a valid option!")
+        except ValueError as e:
+            print(e)
 
 def game_over_coward():
+    """
+    Unlocks the 'coward' ending and
+    has the reset function, which allows players
+    to play another round.
+    """
     print("coward")
 
 def adventure_start():
@@ -29,12 +66,21 @@ def adventure_start():
     print("down?\n")
     
     while True:
-        direction_cave = input("Choose (up/down): ")
+        direction_cave = input("Please choose (up/down): ")
         try:
             if direction_cave.lower() == "up":
+                print("You tighten the grip around")
+                print("your sword. There is no")
+                print("turning back! You will")
+                print("rescue the princess and you")
+                print("will be a hero!\n")
                 room_one()
                 break
             elif direction_cave.lower() == "down":
+                print("Maybe you were not ready")
+                print("for this challenge after all.")
+                print("Your shaking legs seem at")
+                print("at least to think so.")
                 game_over_coward()
                 break
             elif direction_cave == "right":
